851d31b162173cb8b372d5a79100697c9449f296
iljamog <iljamog@tlu.ee>
2021-11-04 16:47:06 +0200

    some

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
     <div className="navbar">
       <div className="navbarLeft">
         <Link to="/">
-          <img className="logo" src="Logo.png" alt="web store logo"/>
+          <img className="logo" src="/shop.png" alt="web store logo"/>
         </Link>
         <div className="navbarMiddle">
           <Link to="/">Tooted</Link>
@@ -14,7 +14,7 @@ function Navbar() {
         </div>
       </div>
       <Link to="cart">
-        <img className="cart" src="cart.svg" alt="cart pic"/>
+        <img className="cart" src="/cart.png" alt="cart pic"/>
       </Link>
     </div>
   );
